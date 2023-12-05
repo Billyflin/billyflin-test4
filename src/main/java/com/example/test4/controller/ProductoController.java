92bdccef5dad1b86c3165131d32c9ca3e8731f33
Billy <billymartinezc@gmail.com>
2023-12-05 16:52:08 -0300

    Initial commit

---
+++ src/main/java/com/example/test4/controller/ProductoController.java
@@ -0,0 +1,42 @@
+package com.example.test4.controller;
+
+import com.example.test4.models.Producto;
+import com.example.test4.service.ProductoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    @PostMapping("/crearProducto")
+    public ResponseEntity<String> verificarProducto(@RequestBody String productoStr) {
+        try {
+            String[] campos = productoStr.replace("\n", "").replace("\r", "").split(",");
+            if (campos.length != 6) {
+                throw new Exception("Formato de entrada inválido");
+            }
+            Producto producto = new Producto();
+            producto.setCodigoProducto(campos[0]);
+            producto.setNombre(campos[1]);
+            producto.setStock(Integer.parseInt(campos[2]));
+            producto.setPrecio(Double.parseDouble(campos[3].replace(".", "")));
+            producto.setRutProveedor(campos[4]);
+            System.out.println(campos[5]);
+            producto.setMailProveedor(campos[5]);
+            productoService.crearProducto(producto);
+            return ResponseEntity.ok("Producto verificado con éxito");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
+
---
+++ src/main/java/com/example/test4/models/Producto.java
@@ -0,0 +1,37 @@
+package com.example.test4.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+public class Producto {
+
+    @Id
+    @Pattern(regexp="^[A-Z]\\d{10}$", message="El código del producto debe comenzar con la inicial de la Marca del automóvil en mayúscula, seguido de la fecha y hora de creación.")
+    private String codigoProducto;
+
+    @Size(max=30, message="El nombre debe contener a lo más 30 letras")
+    private String nombre;
+
+    @Min(value=0, message="El stock debe ser numérico")
+    private int stock;
+
+    @Min(value=0, message="El precio debe ser numérico")
+    private double precio;
+
+    @Pattern(regexp="^\\d{1,2}\\.\\d{3}\\.\\d{3}[-][0-9kK]{1}$", message="Debe ser un rut válido")
+    private String rutProveedor;
+
+    @Email(message="Debe tener un formato válido de mail")
+    private String mailProveedor;
+
+}
